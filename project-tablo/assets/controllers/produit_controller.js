069b581f0b174b55d51374e3994654265af54b2d
michael <michael@cadot.eu>
2025-07-26 17:29:13 +0200

    ajoute modification et création ligne

--- project-tablo/assets/controllers/produit_controller.js
+++ project-tablo/assets/controllers/produit_controller.js
@@ -1,18 +1,20 @@
 import { Controller } from '@hotwired/stimulus';
+
 export default class extends Controller {
-    connect() {
-        const addLineButton = document.getElementById('addline');
-        if (addLineButton) {
-            addLineButton.addEventListener('click', () => {
-                const newRow = document.createElement('tr');
-                newRow.innerHTML = `
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-				`;
-                document.querySelector('tbody').prepend(newRow);
-            });
-        }
+    add() {
+        const newline = this.element.cloneNode(true);
+        newline.children[0].textContent = '';
+        newline.children[1].textContent = '';
+        newline.children[2].textContent = '';
+        this.element.after(newline);
+    }
+    save() {
+        fetch('/save', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id: this.element.children[0].textContent, nom: this.element.children[1].textContent, prix: this.element.children[2].textContent }),
+        })
+            .then(() => location.reload());
     }
 }
+
